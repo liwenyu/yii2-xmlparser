5bb145f1f9785e47cf91c4fbdba051e86e0caddb
Mr.lee <liwenyu66@gmail.com>
2016-09-13 15:53:22 +0800

    init

---
+++ src/Hello.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: liwenyu
+ * Date: 16/9/13
+ * Time: 下午3:50
+ */
+namespace liwenyu\hello;
+
+class Hello
+{
+    public static function sayHello()
+    {
+        return "hello test";
+    }
+}
